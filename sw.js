c56b503091f2143c3bf00c76d05f68d3a9eb1705
“marcos.suhit” <marcos.suhit@almundo.com>
2025-09-27 15:47:47 -0300

    Se eliminan estilos cacheados

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cortes-cache-v1';
+const CACHE_NAME = 'cortes-cache-v3';
 const PRECACHE = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const PRECACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
   );
@@ -15,7 +16,10 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+    Promise.all([
+      caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))),
+      self.clients.claim()
+    ])
   );
 });
 
